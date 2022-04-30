3de61c7ddf92364f688e87b8e0cbb2dc29af6514
Igor <iigormazetti@hotmail.com>
2022-04-30 10:41:08 -0300

    estilização do modal e uso do polished

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -79,4 +79,17 @@ button {
   display: flex;
  }
 
+ .react-modal-close {
+   position: absolute;
+   right: 1.5rem;
+   top: 1.5rem;
+   border: 0;
+   background: transparent;
+   transition: filter 0.2s;
+
+   &:hover {
+     filter: brightness(0.8);
+   }
+ }
+
 `;
